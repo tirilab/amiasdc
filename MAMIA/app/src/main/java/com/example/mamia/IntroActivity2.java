24d293cc7e39bbe9405dbf294020988da51375be
czhan117 <59889383+czhan117@users.noreply.github.com>
2021-09-02 17:07:30 -0400

    intro2 done

--- MAMIA/app/src/main/java/com/example/mamia/IntroActivity2.java
+++ MAMIA/app/src/main/java/com/example/mamia/IntroActivity2.java
@@ -1,5 +1,7 @@
 package com.example.mamia;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class IntroActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro2);
+    }
+
+    public void nextActivity(View v) {
+        Intent i = new Intent(this, IntroActivity3.class);
+        startActivity(i);
+    }
+
+    public void prevActivity(View v) {
+        Intent i = new Intent(this, IntroActivity1.class);
+        startActivity(i);
     }
 }
