fc2df7a1c438384c6b48b62c85226b270740bfc7
czhan117 <59889383+czhan117@users.noreply.github.com>
2021-09-02 23:51:32 -0400

    adding calendar to intro

--- MAMIA/app/src/main/java/com/example/mamia/IntroActivity4.java
+++ MAMIA/app/src/main/java/com/example/mamia/IntroActivity4.java
@@ -1,13 +1,28 @@
 package com.example.mamia;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class IntroActivity4 extends AppCompatActivity {
 
+    CalendarView calendarView;
+    TextView myDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro4);
+
+        calendarView = (CalendarView) findViewById(R.id.intro4_calendar);
+
+    }
+
+    public void nextActivity(View v) {
+        Intent i = new Intent(this, IntroActivity4.class);
+        startActivity(i);
     }
 }
